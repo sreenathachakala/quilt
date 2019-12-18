7b31fa29660f95871ae2722ca0d4d2df2cc74922
armandmcqueen <armandmcqueen@gmail.com>
2019-12-18 15:52:45 -0500

    Quick update

--- create_synthetic_data_packages.py
+++ create_synthetic_data_packages.py
@@ -15,22 +15,27 @@ def create_100kb_pkg():
     create_pkg("100kb")
 
 def create_1mb_pkg():
+    # In progress
     create_pkg("1mb")
 
 def create_10mb_pkg():
+    # Done
     create_pkg("10mb")
 
 def create_100mb_pkg():
+    # Done
     create_pkg("100mb")
 
 def create_1gb_pkg():
+    # In progress
     create_pkg("1gb")
 
 def create_10gb_pkg():
+    # Done!
     create_pkg("10gb")
 
 
 if __name__ == '__main__':
-    create_10gb_pkg()
+    create_1gb_pkg()
 
 
