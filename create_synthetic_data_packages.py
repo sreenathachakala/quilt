6afe2991d97d12a5c62b8bbfad3c8b53e85470a1
armandmcqueen <armandmcqueen@gmail.com>
2019-12-18 09:18:08 -0500

    Quick update

--- create_synthetic_data_packages.py
+++ create_synthetic_data_packages.py
@@ -3,31 +3,31 @@ import quilt3
 def move_nothing(lk, entry):
     return False
 
+def create_pkg(size_str):
+    pkg = quilt3.Package()
+    pkg.set_dir(f"{size_str}/", f"s3://quilt-ml-data/data/download_benchmark/{size_str}/")
+    pkg.push(f"download-benchmark/{size_str}", registry="s3://quilt-ml-data/", selector_fn=move_nothing)
+
 def create_10kb_pkg():
-    pass
+    create_pkg("10kb")
 
 def create_100kb_pkg():
-    pass
+    create_pkg("100kb")
 
 def create_1mb_pkg():
-    pass
+    create_pkg("1mb")
 
 def create_10mb_pkg():
-    pass
-
-
+    create_pkg("10mb")
 
 def create_100mb_pkg():
-    pkg = quilt3.Package()
-    pkg.set_dir("100mb/", "s3://quilt-ml-data/data/download_benchmark/100mb/")
-    pkg.push("download-benchmark/100mb", registry="s3://quilt-ml-data/", selector_fn=move_nothing)
-
+    create_pkg("100mb")
 
 def create_1gb_pkg():
-    pass
+    create_pkg("1gb")
 
 def create_10gb_pkg():
-    pass
+    create_pkg("10gb")
 
 
 if __name__ == '__main__':
