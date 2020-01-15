452d117ddd1818cbcd0c5026bb3e4f38e68e2bf0
armandmcqueen <armandmcqueen@gmail.com>
2020-01-14 22:06:17 -0800

    Point benchmark packages to quilt-example

--- create_synthetic_data_packages.py
+++ create_synthetic_data_packages.py
@@ -3,30 +3,27 @@ import quilt3
 def move_nothing(lk, entry):
     return False
 
-def create_pkg(size_str):
+def create_pkg(file_size_str, dataset_size):
     pkg = quilt3.Package()
-    pkg.set_dir(f"{size_str}/", f"s3://quilt-ml-data/data/download_benchmark/{size_str}/")
-    pkg.push(f"download-benchmark/{size_str}", registry="s3://quilt-ml-data/", selector_fn=move_nothing)
+    pkg.set_dir(f"{file_size_str}/", f"s3://quilt-example/data/random-data-benchmark/{file_size_str}/{dataset_size}")
+    pkg.push(f"random-data-benchmark/{file_size_str}-{dataset_size}", registry="s3://quilt-example/", selector_fn=move_nothing)
 
 
 def create_100kb_pkg():
-    create_pkg("100kb")
+    create_pkg("100kb", "1gb")
 
 def create_1mb_pkg():
-    create_pkg("1mb")
+    create_pkg("1mb", "10gb")
 
 def create_10mb_pkg():
-    create_pkg("10mb")
+    create_pkg("10mb", "10gb")
 
 def create_100mb_pkg():
-    create_pkg("100mb")
+    create_pkg("100mb", "10gb")
 
 def create_1gb_pkg():
-    create_pkg("1gb")
+    create_pkg("1gb", "10gb")
 
-def create_10gb_pkg():
-    # Done!
-    create_pkg("10gb")
 
 
 if __name__ == '__main__':
