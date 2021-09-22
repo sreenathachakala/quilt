592781bf883195c3a4ad6e74182dc36598d729ab
Maxim Chervonny <fiskus@chervonny.ru>
2021-09-22 12:45:42 +0300

    provide config url

--- catalog/app/desktop/index.tsx
+++ catalog/app/desktop/index.tsx
@@ -125,86 +125,90 @@ const sentryUserSelector = (state: $TSFixMe) => {
   return u ? { username: u.current_user, email: u.email } : {}
 }
 
-const configUrl =
-  process.env.NODE_ENV === 'development'
-    ? '/config.json'
-    : 'https://quilt-t4-staging.quiltdata.com/config.json'
+const configPath = '/config.json'
 
-const Root = () => (
-  <M.MuiThemeProvider theme={style.appTheme}>
-    <WithGlobalStyles>
-      <FinalBoundary>
-        <Sentry.Provider>
-          <Store.Provider history={history}>
-            <NamedRoutes.Provider routes={routes}>
-              <RouterProvider history={history}>
-                <Cache.Provider>
-                  <Config.Provider path={configUrl} opts={{ desktop: 'ENABLED' }}>
-                    <React.Suspense fallback={<Placeholder />}>
-                      <Sentry.Loader userSelector={sentryUserSelector}>
-                        <GraphQLProvider>
-                          <ErrorBoundary>
-                            <Notifications.Provider>
-                              <APIConnector.Provider
-                                fetch={fetch}
-                                middleware={[Auth.apiMiddleware]}
-                              >
-                                <Auth.Provider
-                                  checkOn={LOCATION_CHANGE}
-                                  storage={storage}
+const Root = () => {
+  const [host] = React.useState('')
+  const configUrl = React.useMemo(() => {
+    const url = host ? new URL(configPath, host) : configPath
+    return url.toString()
+  }, [host])
+  return (
+    <M.MuiThemeProvider theme={style.appTheme}>
+      <WithGlobalStyles>
+        <FinalBoundary>
+          <Sentry.Provider>
+            <Store.Provider history={history}>
+              <NamedRoutes.Provider routes={routes}>
+                <RouterProvider history={history}>
+                  <Cache.Provider>
+                    <Config.Provider path={configUrl} opts={{ desktop: 'ENABLED' }}>
+                      <React.Suspense fallback={<Placeholder />}>
+                        <Sentry.Loader userSelector={sentryUserSelector}>
+                          <GraphQLProvider>
+                            <ErrorBoundary>
+                              <Notifications.Provider>
+                                <APIConnector.Provider
+                                  fetch={fetch}
+                                  middleware={[Auth.apiMiddleware]}
                                 >
-                                  <Intercom.Provider
-                                    userSelector={intercomUserSelector}
-                                    horizontal_padding={
-                                      // align the launcher with the right side of the container
-                                      (window.innerWidth -
-                                        Math.min(1280, window.innerWidth)) /
-                                        2 +
-                                      32
-                                    }
-                                    vertical_padding={59}
+                                  <Auth.Provider
+                                    checkOn={LOCATION_CHANGE}
+                                    storage={storage}
                                   >
-                                    <ExperimentsProvider>
-                                      <Tracking.Provider
-                                        locationSelector={selectLocation}
-                                        userSelector={Auth.selectors.username}
-                                      >
-                                        <AWS.Credentials.Provider>
-                                          <AWS.Config.Provider>
-                                            <IPCProvider value={IPC}>
-                                              <AWS.Athena.Provider>
-                                                <AWS.S3.Provider>
-                                                  <Notifications.WithNotifications>
-                                                    <ErrorBoundary>
-                                                      <BucketCacheProvider>
-                                                        <DesktopApp />
-                                                      </BucketCacheProvider>
-                                                    </ErrorBoundary>
-                                                  </Notifications.WithNotifications>
-                                                </AWS.S3.Provider>
-                                              </AWS.Athena.Provider>
-                                            </IPCProvider>
-                                          </AWS.Config.Provider>
-                                        </AWS.Credentials.Provider>
-                                      </Tracking.Provider>
-                                    </ExperimentsProvider>
-                                  </Intercom.Provider>
-                                </Auth.Provider>
-                              </APIConnector.Provider>
-                            </Notifications.Provider>
-                          </ErrorBoundary>
-                        </GraphQLProvider>
-                      </Sentry.Loader>
-                    </React.Suspense>
-                  </Config.Provider>
-                </Cache.Provider>
-              </RouterProvider>
-            </NamedRoutes.Provider>
-          </Store.Provider>
-        </Sentry.Provider>
-      </FinalBoundary>
-    </WithGlobalStyles>
-  </M.MuiThemeProvider>
-)
+                                    <Intercom.Provider
+                                      userSelector={intercomUserSelector}
+                                      horizontal_padding={
+                                        // align the launcher with the right side of the container
+                                        (window.innerWidth -
+                                          Math.min(1280, window.innerWidth)) /
+                                          2 +
+                                        32
+                                      }
+                                      vertical_padding={59}
+                                    >
+                                      <ExperimentsProvider>
+                                        <Tracking.Provider
+                                          locationSelector={selectLocation}
+                                          userSelector={Auth.selectors.username}
+                                        >
+                                          <AWS.Credentials.Provider>
+                                            <AWS.Config.Provider>
+                                              <IPCProvider value={IPC}>
+                                                <AWS.Athena.Provider>
+                                                  <AWS.S3.Provider>
+                                                    <Notifications.WithNotifications>
+                                                      <ErrorBoundary>
+                                                        <BucketCacheProvider>
+                                                          <DesktopApp />
+                                                        </BucketCacheProvider>
+                                                      </ErrorBoundary>
+                                                    </Notifications.WithNotifications>
+                                                  </AWS.S3.Provider>
+                                                </AWS.Athena.Provider>
+                                              </IPCProvider>
+                                            </AWS.Config.Provider>
+                                          </AWS.Credentials.Provider>
+                                        </Tracking.Provider>
+                                      </ExperimentsProvider>
+                                    </Intercom.Provider>
+                                  </Auth.Provider>
+                                </APIConnector.Provider>
+                              </Notifications.Provider>
+                            </ErrorBoundary>
+                          </GraphQLProvider>
+                        </Sentry.Loader>
+                      </React.Suspense>
+                    </Config.Provider>
+                  </Cache.Provider>
+                </RouterProvider>
+              </NamedRoutes.Provider>
+            </Store.Provider>
+          </Sentry.Provider>
+        </FinalBoundary>
+      </WithGlobalStyles>
+    </M.MuiThemeProvider>
+  )
+}
 
 ReactDOM.render(<Root />, MOUNT_NODE)
