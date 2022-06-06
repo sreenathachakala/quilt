9550b8f8ab79fad380c7e61772d751990893a34e
Maxim Chervonny <fiskus@chervonny.ru>
2022-06-06 17:32:03 +0200

    add additional log events on event handlers

--- catalog/app/utils/electron/ipc.ts
+++ catalog/app/utils/electron/ipc.ts
@@ -2,10 +2,16 @@ import { ipcRenderer, IpcRendererEvent } from 'electron'
 
 export * as EVENTS from './events'
 
+const debug = (...args: any[]) => {
+  // eslint-disable-next-line no-console
+  console.debug(...args)
+}
+
 export function off(
   eventName: string,
   callback: (event: IpcRendererEvent, ...args: any[]) => void,
 ) {
+  debug('Off', eventName)
   ipcRenderer.off(eventName, callback)
 }
 
@@ -13,14 +19,18 @@ export function on(
   eventName: string,
   callback: (event: IpcRendererEvent, ...args: any[]) => void,
 ) {
+  // TODO: add debug to callback, return new function wrapper for callback
+  //       and use wrapper to unsubscribe
   ipcRenderer.on(eventName, callback)
 }
 
 export function invoke(channel: string, ...args: any[]) {
+  debug('Invoke', channel, '. Arguments:', args)
   return ipcRenderer.invoke(channel, ...args)
 }
 
 export function send(channel: string, ...args: any[]) {
+  debug('Send', channel, '. Arguments:', args)
   ipcRenderer.send(channel, ...args)
   return Promise.resolve(null)
 }
